69a6f8beb22fd4ce6d8d140be6fb092dfd6ed8e4
tabish <tabishimam609@outlook.com>
2026-01-29 14:55:32 +0530

    added application

--- Application/variables.tf
+++ Application/variables.tf
@@ -7,11 +7,13 @@ variable "aws_region" {
 variable "vpc_id" {
   description = "VPC ID for ECS cluster"
   type        = string
+  default = "vpc-033d3f73769d2bfe1"
 }
 
 variable "private_subnets" {
   description = "Private subnets for ECS tasks"
   type        = list(string)
+  default = ["subnet-0d007597e8d8be217", "subnet-015ed8decd8bdea82"]
 }
 
 variable "desired_count" {
